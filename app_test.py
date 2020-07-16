9e140aed3c154e0aedf6d01d97200dc0db37b4b9
mdan <t.t.mina16@gmail.com>
2020-07-17 07:27:20 +0900

    first commit

---
+++ app_test.py
@@ -0,0 +1,15 @@
+from app import hello
+from app import test
+import unittest
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        expected = "Hello World"
+        self.assertEqual(expected, hello())
+
+    def test_test(self):
+        expected = "Test"
+        self.assertEqual(expected, test())
+
+if __name__ == "__main__":
+    unittest.main()
